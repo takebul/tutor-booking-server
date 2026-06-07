6ac2602f07333523260c7c217e31aeab49dc0358
takebul <takebulislam@gmail.com>
2026-06-08 01:31:17 +0600

    added tutorData in mongodb client side

--- index.js
+++ index.js
@@ -4,14 +4,47 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 8541;
-dotenv.config();
 
+dotenv.config();
 app.use(cors());
 app.use(express.json());
 
+const uri = process.env.MONGODB_URI;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // await client.connect();
+    // await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!",
+    );
+
+    const db = client.db("TutorBooking");
+    const tutorDataCollection = db.collection("tutorData");
+
+    app.get("/tutor", async (req, res) => {
+      const result = await tutorDataCollection.find().toArray();
+      res.send(result);
+    });
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 app.get("/", (req, res) => {
-  res.send("Server is running fine");
+  res.send("server is running fine");
 });
 
 app.listen(port, () => {
